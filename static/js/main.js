2e5045801f505d032bfe37cf3bf007495162b100
Lokesha Shetty <lokesha.shetty@SPI.COM>
2018-04-19 13:04:28 +0530

    Intial project files - static

---
+++ static/js/main.js
@@ -0,0 +1,19 @@
+// Image carousel
+(function() {
+
+    let slideIndex = 1;
+
+    let showSlide = (n) => {
+        
+        const carousel__slide = document.querySelectorAll(".carousel__slide");
+        n > carousel__slide.length && (slideIndex = 1);
+        n < 1 && (slideIndex = carousel__slide.length);
+        carousel__slide.forEach((elem, index) => elem.style.display = "none");
+        carousel__slide[slideIndex - 1].style.display = "block";
+    }
+
+    showSlide(slideIndex);
+    let goToSlide = (n) => showSlide(slideIndex += n);
+    document.querySelector('.carousel__button--left').addEventListener('click', () => goToSlide(-1));
+    document.querySelector('.carousel__button--right').addEventListener('click', () => goToSlide(1));
+}());
